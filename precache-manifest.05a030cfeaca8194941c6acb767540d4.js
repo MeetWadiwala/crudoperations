92e7f9749ec44724bdfac35b063ac5fae9802103
MeetWadiwala <71484359+MeetWadiwala@users.noreply.github.com>
2020-10-15 20:40:57 +0530

    Updates

---
+++ precache-manifest.05a030cfeaca8194941c6acb767540d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d5c9e0c27379fef419bbb627735fca",
+    "url": "/crudoperations/index.html"
+  },
+  {
+    "revision": "c638fa401371eebe539b",
+    "url": "/crudoperations/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "de081af101c26eafc6cd",
+    "url": "/crudoperations/static/css/main.bd0f38d7.chunk.css"
+  },
+  {
+    "revision": "c638fa401371eebe539b",
+    "url": "/crudoperations/static/js/2.eb0268f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/crudoperations/static/js/2.eb0268f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de081af101c26eafc6cd",
+    "url": "/crudoperations/static/js/main.2d4ba888.chunk.js"
+  },
+  {
+    "revision": "ebf81b1cf2fc5e808c11",
+    "url": "/crudoperations/static/js/runtime-main.78461729.js"
+  }
+]);
